99350e7722c9081d26c3acf113c1fd01f412e700
Sammy <53941165+azxnexception@users.noreply.github.com>
2024-09-26 09:24:08 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ repositories {
 
 dependencies {
     implementation(platform("com.intellectualsites.bom:bom-newest:1.44"))
-    compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
     compileOnly("net.md-5:bungeecord-api:1.20-R0.2")
     compileOnly("com.mojang:authlib:1.5.25")
     compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Bukkit")
